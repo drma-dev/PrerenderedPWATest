3257a38fe9a4a092900adcd1af1e8f00d018a464
Dhiogo Acioli <dhiogoacioli@outlook.com>
2023-09-27 22:06:39 -0300

    first try

--- PrerenderedPWATest/Program.cs
+++ PrerenderedPWATest/Program.cs
@@ -1,8 +1,16 @@
+using AzureStaticWebApps.Blazor.Authentication;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using BlazorPro.BlazorSize;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PrerenderedPWATest;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 if (!builder.RootComponents.Any())
 {
@@ -10,6 +18,20 @@ if (!builder.RootComponents.Any())
     builder.RootComponents.Add<HeadOutlet>("head::after");
 }
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection collection, string baseAddress)
+{
+    collection
+            .AddBlazorise(options => options.Immediate = true)
+            .AddBootstrapProviders()
+            .AddFontAwesomeIcons();
+
+    collection.AddPWAUpdater();
+    collection.AddMediaQueryService();
+    collection.AddMemoryCache();
+
+    collection.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+    collection.AddStaticWebAppsAuthentication();
+}
